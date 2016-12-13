86f4d64ef5d4fff32214c60c4b971f867b433b26
Michal Kovacik <xkovacikm2@gmail.com>
2016-12-13 16:51:27 +0100

    loading finished

--- main.cpp
+++ main.cpp
@@ -5,13 +5,14 @@
 #include <vector>
 #include <thread>
 #include <fstream>
+#include <cstdlib>
 #include "sha1.hpp"
 
 bool exit_flag = false;
-constexpr const size_t brute_force = 7;
+constexpr const size_t brute_force = 8;
 constexpr const size_t chain_size = 1000;
 
-std::string reduce(std::string &hash, size_t n) {
+std::string reduce(std::string hash, size_t n) {
   std::string digits_from_hash;
   std::copy_if(hash.begin(), hash.end(), std::back_inserter(digits_from_hash), isdigit);
   std::string result(digits_from_hash);
@@ -21,19 +22,40 @@ std::string reduce(std::string &hash, size_t n) {
   return result.substr(0, n);
 }
 
-void rainbows_and_unicorns(uint64_t n, std::vector<std::pair<std::string, std::string>> &vec){
-  SHA1 sha;
-  if (n<brute_force){
+void rainbows_and_unicorns(uint64_t n, std::vector<std::pair<std::string, std::string>> &vec) {
+  if (n < brute_force) {
     uint64_t n_max = pow(10, n);
-    for(uint64_t i = (n==1) ? 0 : pow(10, n-1); i< n_max && !exit_flag; i++){
-      std::string i_str = std::to_string(i);
+    for (uint64_t i = (n == 1) ? 0 : pow(10, n - 1); i < n_max && !exit_flag; i++) {
+      auto i_str = std::to_string(i);
+      SHA1 sha;
       sha.update(i_str);
       vec.push_back({sha.final(), i_str});
     }
   } else {
-
+    uint64_t repeats = pow(10, n) / chain_size;
+    for (uint64_t i = 0; i < repeats && !exit_flag; i++) {
+      std::string pass;
+      for (size_t j = 0; j < n; j++) {
+        pass += std::to_string(rand() % 10);
+      }
+      auto tmp = pass;
+      std::string hash;
+      for (uint64_t j = 0; j < chain_size; j++) {
+        SHA1 sha;
+        sha.update(tmp);
+        hash = sha.final();
+        tmp = reduce(hash, n);
+      }
+      vec.push_back({hash, pass});
+    }
   }
   std::cout << "done " << n << std::endl;
+}
+
+void quit_on_q() {
+  while (getchar() != 'q');
+  exit_flag = true;
+  std::cout << "acknowledged, initiating serialization" << std::endl;
 }
 
 void create_m(int n, std::string filename) {
@@ -43,30 +65,28 @@ void create_m(int n, std::string filename) {
   }
 
   std::vector<std::thread> threads;
-  for (int i = 0; i < n; i++){
-    threads.push_back(std::thread(rainbows_and_unicorns, i+1, std::ref(top_map[i])));
-  }
-
-  while(getchar() != 'q');
-  exit_flag = true;
-
-  for(auto &thread : threads){
-    thread.join();
+  for (int i = 0; i < n; i++) {
+    threads.push_back(std::thread(rainbows_and_unicorns, i + 1, std::ref(top_map[i])));
   }
 
   std::ofstream file(filename);
-  if(!file.is_open()){
+  if (!file.is_open()) {
     throw std::runtime_error("cannot open file with such name");
   }
 
+  std::thread kek(quit_on_q);
+
   file << n << '\n' << '\n';
-  for(auto &vec : top_map){
-    std::sort(vec.begin(), vec.end());
-    for(auto &pair : vec){
+
+  for (size_t i = 0; i<n; i++) {
+    threads[i].join();
+    std::sort(top_map[i].begin(), top_map[i].end());
+    for (auto &pair : top_map[i]) {
       file << pair.first << ',' << pair.second << '\n';
     }
     file << '\n';
   }
+  kek.join();
 }
 
 int main(int argc, char *argv[]) {
